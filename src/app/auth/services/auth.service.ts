a47fcdc0f405d92e0c7db5f82e8c81f594501ecd
Natanael Terreno <j.terreno@hotmail.com>
2021-11-21 11:05:54 -0300

    final del proyecto

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 import { Auth } from '../interfaces/auth.interfaces';
@@ -10,10 +12,38 @@ import { Auth } from '../interfaces/auth.interfaces';
 export class AuthService {
 
   private endpointAPI: string = environment.endpointAPI;
+  private _auth: Auth | undefined;
+
+  get auth(): Auth{
+    return {...this._auth!}
+  }
 
   constructor(private http: HttpClient) { }
 
+  verificaAutenticacion(): Observable<boolean>{
+
+    if (!localStorage.getItem('token')) {
+      return of(false);
+    }
+    
+    return this.http.get<Auth>(`${this.endpointAPI}/usuarios/1`)
+      .pipe(
+        map(auth => {
+          this._auth = auth;
+          return true;
+        })
+      );
+  }
+
   login(){
-    return this.http.get<Auth>(`${this.endpointAPI}/usuarios/17`);
+    return this.http.get<Auth>(`${this.endpointAPI}/usuarios/1`)
+        .pipe(
+          tap(auth => this._auth = auth),
+          tap(auth => localStorage.setItem('token',auth.id))
+          );
+  }
+
+  logout(){
+    this._auth = undefined;
   }
 }
